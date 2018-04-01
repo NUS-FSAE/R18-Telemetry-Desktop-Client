be2115f2304f3d89f17ffa1dfce566d30b7fc899
HaoYe <haoye94@hotmail.com>
2018-04-02 00:03:56 +0800

    Updated to show brake temperatures instead of oil and fuel pressure
    Fixed the problem of data becoming negative

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,12 @@ import eu.hansolo.tilesfx.tools.FlowGridPane;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Stop;
@@ -22,12 +24,14 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import sample.udpListener;
-
+import javafx.animation.AnimationTimer;
 import java.awt.event.ActionEvent;
 import java.beans.EventHandler;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class Main extends Application {
     private static final double TILE_SIZE = 200;
@@ -35,8 +39,26 @@ public class Main extends Application {
     private static final double LOW_OIL_PRESSURE = 70;
     private static final double MAX_BRAKE_PRESSURE = 5.0;
 
+    private double rpm;
+    private double oilPressure;
+    private double fuelPressure;
+    private double engTemp;
+    private double oilTemp;
+    private double battVolts;
+    private double speed;
+    private double throttle;
+    private double brake;
+    private String gear;
+    private double brakeTempFront;
+    private double brakeTempRear;
+
     udpListener listener;
-    byte[] data1,data2,data3,data4,data5;
+    private byte[] data1 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private byte[] data2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private byte[] data3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private byte[] data4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private byte[] data5 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private Label oilPressureLabel;
 
     private Gauge vehicleSpeedGauge;
     private Gauge rpmGauge;
@@ -45,6 +67,8 @@ public class Main extends Application {
     private Gauge fuelGauge;
     private Gauge throttlePositionGauge;
     private Gauge brakePressureGauge;
+    private Gauge brakeTempFrontGauge;
+    private Gauge brakeTempRearGauge;
 
     private Tile vehicleSpeedTile;
     private Tile rpmTile;
@@ -56,9 +80,12 @@ public class Main extends Application {
     private Tile batteryTile;
     private Tile oilTempTile;
     private Tile brakePressureTile;
+    private Tile brakeTempFrontTile;
+    private Tile brakeTempRearTile;
 
     @Override
     public void init() {
+
         //Vehicle Speed
         //
         //
@@ -67,7 +94,7 @@ public class Main extends Application {
         vehicleSpeedGauge.setMinValue(0.0);
         vehicleSpeedGauge.setMaxValue(120.0);
         vehicleSpeedGauge.setDecimals(0);
-        vehicleSpeedTile  = TileBuilder.create()
+        vehicleSpeedTile = TileBuilder.create()
                 .prefSize(TILE_SIZE, TILE_SIZE)
                 .skinType(Tile.SkinType.CUSTOM)
                 .title("Speed")
@@ -82,7 +109,7 @@ public class Main extends Application {
         rpmGauge.setMinValue(0.0);
         rpmGauge.setMaxValue(12000.0);
         rpmGauge.setDecimals(0);
-        rpmTile  = TileBuilder.create()
+        rpmTile = TileBuilder.create()
                 .prefSize(TILE_SIZE, TILE_SIZE)
                 .skinType(Tile.SkinType.CUSTOM)
                 .title("RPM")
@@ -120,20 +147,49 @@ public class Main extends Application {
                 .strokeWithGradient(true)
                 .gradientStops(new Stop(0.0, Tile.LIGHT_GREEN),
                         new Stop(0.33, Tile.LIGHT_GREEN),
-                        new Stop(0.33,Tile.YELLOW),
+                        new Stop(0.33, Tile.YELLOW),
                         new Stop(0.67, Tile.YELLOW),
                         new Stop(0.67, Tile.LIGHT_RED),
                         new Stop(1.0, Tile.LIGHT_RED))
                 .value(252)
                 .build();
 
+        brakeTempFrontGauge = createGauge(Gauge.SkinType.SIMPLE_SECTION);
+        brakeTempFrontGauge.setBarColor(Color.BLUE);
+        brakeTempFrontGauge.setSections(new Section(300, 450, Tile.RED));
+        brakeTempFrontGauge.setMaxValue(450);
+        brakeTempFrontGauge.setMinValue(0);
+        brakeTempFrontGauge.setDecimals(0);
+        brakeTempFrontTile = TileBuilder.create()
+                .prefSize(TILE_SIZE, TILE_SIZE)
+                .skinType(Tile.SkinType.CUSTOM)
+                .text("Brake Temperature Front")
+                .graphic(brakeTempFrontGauge)
+                .build();
+        brakeTempFrontGauge.setValue(20);
+
+        brakeTempRearGauge = createGauge(Gauge.SkinType.SIMPLE_SECTION);
+        brakeTempRearGauge.setBarColor(Color.BLUE);
+        brakeTempRearGauge.setSections(new Section(300, 450, Tile.RED));
+        brakeTempRearGauge.setMaxValue(450);
+        brakeTempRearGauge.setMinValue(0);
+        brakeTempRearGauge.setDecimals(0);
+        brakeTempRearTile = TileBuilder.create()
+                .prefSize(TILE_SIZE, TILE_SIZE)
+                .skinType(Tile.SkinType.CUSTOM)
+                .text("Brake Temperature Rear")
+                .graphic(brakeTempRearGauge)
+                .build();
+        brakeTempRearGauge.setValue(20);
+
+
         //Throttle position
         //
         //
         throttlePositionGauge = createGauge(Gauge.SkinType.CHARGE);
         throttlePositionGauge.setValue(0.5);
-        throttlePositionTile  = TileBuilder.create()
-                .prefSize(TILE_SIZE*2, TILE_SIZE)
+        throttlePositionTile = TileBuilder.create()
+                .prefSize(TILE_SIZE * 2, TILE_SIZE)
                 .skinType(Tile.SkinType.CUSTOM)
                 .title("Throttle Position")
                 .graphic(throttlePositionGauge)
@@ -144,8 +200,8 @@ public class Main extends Application {
         //
         brakePressureGauge = createGauge(Gauge.SkinType.CHARGE);
         brakePressureGauge.setValue(0.5);
-        brakePressureTile  = TileBuilder.create()
-                .prefSize(TILE_SIZE*2, TILE_SIZE)
+        brakePressureTile = TileBuilder.create()
+                .prefSize(TILE_SIZE * 2, TILE_SIZE)
                 .skinType(Tile.SkinType.CUSTOM)
                 .title("Brake Pressure")
                 .graphic(brakePressureGauge)
@@ -161,7 +217,7 @@ public class Main extends Application {
         fuelGauge.setMinValue(0);
         fuelGauge.setUnit("kPa");
         fuelGauge.setDecimals(0);
-        fuelTile  = TileBuilder.create()
+        fuelTile = TileBuilder.create()
                 .prefSize(TILE_SIZE, TILE_SIZE)
                 .skinType(Tile.SkinType.CUSTOM)
                 .title("Fuel Pressure")
@@ -178,7 +234,7 @@ public class Main extends Application {
         oilTempGauge.setMaxValue(130);
         oilTempGauge.setMinValue(0);
         oilTempGauge.setDecimals(0);
-        oilTempTile  = TileBuilder.create()
+        oilTempTile = TileBuilder.create()
                 .prefSize(TILE_SIZE, TILE_SIZE)
                 .skinType(Tile.SkinType.CUSTOM)
                 .text("Oil Temperature")
@@ -195,7 +251,7 @@ public class Main extends Application {
         engTempGauge.setMaxValue(130);
         engTempGauge.setMinValue(0);
         engTempGauge.setDecimals(0);
-        engTempTile  = TileBuilder.create()
+        engTempTile = TileBuilder.create()
                 .prefSize(TILE_SIZE, TILE_SIZE)
                 .skinType(Tile.SkinType.CUSTOM)
                 .text("Engine Temperature")
@@ -217,57 +273,144 @@ public class Main extends Application {
                 .build();
         batteryTile.setValue(12.9);
 
+        oilPressureLabel = new Label("nil");
+
         listener = new udpListener();
         Thread thread = new Thread(listener);
         thread.setDaemon(true);
         thread.setPriority(Thread.MAX_PRIORITY);
         thread.start();
 
-        new Timer().schedule(
-                new TimerTask() {
-                    @Override
-                    public void run() {
-                        synchronized (udpListener.lock) {
-                            data1 = listener.getBuffer1();
-                            data2 = listener.getBuffer2();
-                            data3 = listener.getBuffer3();
-                            data4 = listener.getBuffer4();
-                            data5 = listener.getBuffer5();
-                        }
-//                        if (udpListener.received) {
-//                            batteryTile.setValue(data1[0]);
-//                            udpListener.received = false;
-//                        }
+        AnimationTimer timer = new AnimationTimer() {
 
-                            rpmGauge.setValue(data1[1] << 8 | data1[2]);
-                            oilPressTile.setValue(data1[3] << 8 | data1[4]);
-                            fuelGauge.setValue(data1[5] << 8 | data1[6]);
-                            throttlePositionGauge.setValue(data1[7] / 100.0);
-                            vehicleSpeedGauge.setValue(data1[8]);
-                            brakePressureGauge.setValue(data2[1] / MAX_BRAKE_PRESSURE);
-                            //   gearTile.setDescription(Integer.toString((int)data2[6]));
-                            engTempGauge.setValue((double) data3[1]);
-                            oilTempGauge.setValue((double) data3[2]);
-                            batteryTile.setValue((data3[3] & 0x000000FF) / 10.0); //convert to unsigned int
+            @Override
+            public void handle(long now) {
+                synchronized (udpListener.lock) {
+                    if (listener.buffer1_isReceived()) {
+                        data1 = listener.getBuffer1();
+                        listener.setBuffer1_read();
                     }
-                }, 10,30);
+                    if (listener.buffer2_isReceived()) {
+                        data2 = listener.getBuffer2();
+                        listener.setBuffer2_read();
+                    }
+                    if (listener.buffer3_isReceived()) {
+                        data3 = listener.getBuffer3();
+                        listener.setBuffer3_read();
+                    }
+                    if (listener.buffer4_isReceived()) {
+                        data4 = listener.getBuffer4();
+                        listener.setBuffer4_read();
+                    }
+                    if (listener.buffer5_isReceived()) {
+                        data5 = listener.getBuffer5();
+                        listener.setBuffer5_read();
+                    }
+
+                    rpm = ((data1[1] << 8) | data1[2] & 0xFF);
+                    oilPressure = (((data1[3] << 8) | data1[4] & 0xFF )/10);
+                    fuelPressure = ((data1[5] << 8 | data1[6] & 0xFF) / 10);
+                    throttle = data1[7] / 100.0;
+                    speed = data1[8];
+                    brake = data2[1] / MAX_BRAKE_PRESSURE;
+                    brakeTempFront = (data2[3] << 8 | data2[4] & 0xFF);
+                    brakeTempRear = (data2[5] << 8 | data2[6] & 0xFF);
+                    engTemp = (double) data3[1];
+                    oilTemp = (double) data3[2];
+                    gear = Integer.toString((int) data2[7]);
+                    battVolts = (data3[3] & 0x000000FF) / 10.0;
+
+                    rpmGauge.setValue(rpm);
+                    oilPressTile.setValue(oilPressure);
+                    fuelGauge.setValue(fuelPressure);
+                    brakeTempFrontGauge.setValue(brakeTempFront);
+                    brakeTempRearGauge.setValue(brakeTempRear);
+                    throttlePositionGauge.setValue(throttle);
+                    vehicleSpeedGauge.setValue(speed);
+                    brakePressureGauge.setValue(brake);
+                    engTempGauge.setValue(engTemp);
+                    gearTile.setDescription(gear);
+                    oilTempGauge.setValue(oilTemp);
+                    batteryTile.setValue(battVolts); //convert to unsigned int
+                }
+            }
+
+        };
+
+        timer.start();
+
+//        new Timer().schedule(
+//                listener,10,10);
+//                new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        synchronized (udpListener.lock) {
+//                            if(listener.buffer1_isReceived()) {
+//                                data1 = listener.getBuffer1();
+//                                listener.setBuffer1_read();
+//                            }
+//                            if(listener.buffer2_isReceived()) {
+//                                data2 = listener.getBuffer2();
+//                                listener.setBuffer2_read();
+//                            }
+//                            if(listener.buffer3_isReceived()) {
+//                                data3 = listener.getBuffer3();
+//                                listener.setBuffer3_read();
+//                            }
+//                            if(listener.buffer4_isReceived()) {
+//                                data4 = listener.getBuffer4();
+//                                listener.setBuffer4_read();
+//                            }
+//                            if(listener.buffer5_isReceived()) {
+//                                data5 = listener.getBuffer5();
+//                                listener.setBuffer5_read();
+//                            }
+//                            try {
+//                                rpm = data1[1] << 8 | data1[2];
+//                                oilPressure = data1[3] << 8 | data1[4];
+//                                fuelPressure = (data1[5] << 8 | data1[6])/10.0;
+//                                throttle = data1[7] / 100.0;
+//                                speed = data1[8];
+//                                brake = data2[1] / MAX_BRAKE_PRESSURE;
+//                                engTemp = (double) data3[1];
+//                                oilTemp = (double) data3[2];
+//                                gear = Integer.toString((int)data2[6]);
+//                                battVolts = (data3[3] & 0x000000FF) / 10.0;
+//                            } catch (Exception e) {
+//                                System.console().printf("No!!!!");
+//                                //e.printStackTrace();
+//                            }
+//                            rpmGauge.setValue(rpm);
+//                            oilPressTile.setValue(oilPressure);
+////                            oilPressureLabel.setText(Double.toString(battVolts));
+//                            fuelGauge.setValue(fuelPressure);
+//                            throttlePositionGauge.setValue(throttle);
+//                            vehicleSpeedGauge.setValue(speed);
+//                            brakePressureGauge.setValue(brake);
+//                            engTempGauge.setValue(engTemp);
+////                            gearTile.setDescription(gear);
+//                            oilTempGauge.setValue(oilTemp);
+//                            batteryTile.setValue(battVolts); //convert to unsigned int
+//                        }
+//                    }
+//                }, 10,10);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         VBox rootNode = new VBox();
-        HBox     row_1 = new HBox();
-        HBox     row_2 = new HBox();
-        HBox     row_3 = new HBox();
-        HBox     row_4 = new HBox();
+        HBox row_1 = new HBox();
+        HBox row_2 = new HBox();
+        HBox row_3 = new HBox();
+        HBox row_4 = new HBox();
 
-        row_1.getChildren().addAll(vehicleSpeedTile,rpmTile,gearTile);
-        row_2.getChildren().addAll(oilPressTile,throttlePositionTile);
-        row_3.getChildren().addAll(fuelTile,brakePressureTile);
-        row_4.getChildren().addAll(batteryTile,engTempTile,oilTempTile);
+        row_1.getChildren().addAll(vehicleSpeedTile, rpmTile, gearTile);
+        row_2.getChildren().addAll(brakeTempFrontTile, throttlePositionTile);
+        row_3.getChildren().addAll(brakeTempRearTile, brakePressureTile);
+        row_4.getChildren().addAll(batteryTile, engTempTile, oilTempTile);
 
-        rootNode.getChildren().addAll(row_1,row_2,row_3,row_4);
-        Scene scene = new Scene(rootNode,TILE_SIZE*3,TILE_SIZE*4);
+        rootNode.getChildren().addAll(row_1, row_2, row_3, row_4);
+        Scene scene = new Scene(rootNode, TILE_SIZE * 3, TILE_SIZE * 4);
         primaryStage.setTitle("Sample Application");
         primaryStage.setScene(scene);
         primaryStage.show();
--- src/sample/udpListener.java
+++ src/sample/udpListener.java
@@ -1,11 +1,16 @@
 package sample;
 import java.io.*;
 import java.net.*;
+import java.util.TimerTask;
 
-public class udpListener implements Runnable{
-    public static Boolean received = false;
+public class udpListener extends TimerTask {
     private static final int PORT = 8018;
-    private static final int PACKET_SIZE = 9;
+    private static final int PACKET_SIZE = 10;
+    private static boolean buffer1_received = false;
+    private static boolean buffer2_received = false;
+    private static boolean buffer3_received = false;
+    private static boolean buffer4_received = false;
+    private static boolean buffer5_received = false;
     private byte[] data = new byte[PACKET_SIZE];
     private byte[] buffer1 = new byte[PACKET_SIZE];
     private byte[] buffer2 = new byte[PACKET_SIZE];
@@ -27,18 +32,23 @@ public class udpListener implements Runnable{
                         switch (packet.getData()[0]) {
                             case 0x40:
                                 System.arraycopy(packet.getData(),0,buffer1,0,PACKET_SIZE);
+                                buffer1_received = true;
                                 break;
                             case 0x41:
                                 System.arraycopy(packet.getData(),0,buffer2,0,PACKET_SIZE);
+                                buffer2_received = true;
                                 break;
                             case 0x42:
                                 System.arraycopy(packet.getData(),0,buffer3,0,PACKET_SIZE);
+                                buffer3_received = true;
                                 break;
                             case 0x43:
                                 System.arraycopy(packet.getData(),0,buffer4,0,PACKET_SIZE);
+                                buffer4_received = true;
                                 break;
                             case 0x44:
                                 System.arraycopy(packet.getData(),0,buffer5,0,PACKET_SIZE);
+                                buffer5_received = true;
                                 break;
                             default:
                                 break;
@@ -69,6 +79,18 @@ public class udpListener implements Runnable{
     public byte[] getBuffer5() {
         return buffer5;
     }
+
+    public boolean buffer1_isReceived() { return buffer1_received; }
+    public boolean buffer2_isReceived() { return buffer2_received; }
+    public boolean buffer3_isReceived() { return buffer3_received; }
+    public boolean buffer4_isReceived() { return buffer4_received; }
+    public boolean buffer5_isReceived() { return buffer5_received; }
+
+    public void setBuffer1_read() { buffer1_received = false; }
+    public void setBuffer2_read() { buffer2_received = false; }
+    public void setBuffer3_read() { buffer3_received = false; }
+    public void setBuffer4_read() { buffer4_received = false; }
+    public void setBuffer5_read() { buffer5_received = false; }
 
 
 }
